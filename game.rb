c82bdddec7dc5dfdf9e6463cabdfbc716180171c
Richard Ten <rten04@icloud.com>
2022-12-11 16:28:14 -0500

    Add methods/tests
    Adding game intro (rules, players, etc.)

--- game.rb
+++ game.rb
@@ -1,8 +1,33 @@
-require './players.rb'
+require './display.rb'
 
 class Game
+    include Display
+
+    attr_accessor :gameOver, :winner
+
+    @@board_spaces = {
+        1 => '1',
+        2 => '2',
+        3 => '3',
+        4 => '4',
+        5 => '5',
+        6 => '6',
+        7 => '7', 
+        8 => '8',
+        9 => '9'
+    }
+
     def initialize(player1, player2)
         @p1 = player1
         @p2 = player2
+        @gameOver = false
+        @winner = false
+        @ava
+    end
+
+    def play
+        displayRules
+        displayPlayers(@p1.player, @p2.player)
+        displayBoard(@@board_spaces)
     end
 end

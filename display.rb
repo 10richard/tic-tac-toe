f890553de0807260576738ea85d59bf07582823c
Richard Ten <rten04@icloud.com>
2022-12-11 16:27:53 -0500

    Add methods

--- display.rb
+++ display.rb
@@ -1,3 +1,16 @@
 module Display
-    
+    def displayBoard(board_spaces)
+        puts "#{board_spaces[1]} | #{board_spaces[2]} | #{board_spaces[3]}"
+        puts "#{board_spaces[4]} | #{board_spaces[5]} | #{board_spaces[6]}"
+        puts "#{board_spaces[7]} | #{board_spaces[8]} | #{board_spaces[9]}"
+    end
+
+    def displayPlayers(player1, player2)
+        puts "#{player1} is 'X'"
+        puts "#{player2} is 'O'"
+    end
+
+    def displayRules
+        puts "Enter a number corresponding to an available space on the board"
+    end
 end

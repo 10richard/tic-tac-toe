12a07484560f7f5bd4b39dcfb9c14315659952e0
Richard Ten <rten04@icloud.com>
2022-12-11 17:00:30 -0500

    Add methods
    Added methods that get a move from a user then checks if it is available
    on the board

--- game.rb
+++ game.rb
@@ -17,17 +17,45 @@ class Game
         9 => '9'
     }
 
+    @@available_spaces = @@board_spaces.values
+    @@p1_choices = []
+    @@p2_choices = []
+
     def initialize(player1, player2)
         @p1 = player1
         @p2 = player2
         @gameOver = false
         @winner = false
-        @ava
     end
 
     def play
         displayRules
         displayPlayers(@p1.player, @p2.player)
-        displayBoard(@@board_spaces)
+        puts
+        currentPlayer = @p1
+        
+        until gameOver
+            displayBoard(@@board_spaces)
+            puts "#{currentPlayer.player}'s turn"
+            move = checkMove(getMove)
+            @@board_spaces[move.to_i] =  currentPlayer == @p1 ? 'X' : 'O'
+            
+            @gameOver = true
+        end
+    end
+
+    def getMove
+        move = gets.chomp
+        return move
+    end
+
+    def checkMove(move)
+        if @@available_spaces.include?(move)
+            @@available_spaces.delete(move)
+        else
+            puts "You did not enter an open space on the board"
+            puts "Please try again."
+            getMove
+        end
     end
 end
